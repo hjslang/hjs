79334ec7ba73746c7894dc5d25c974b52546a2be
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-08-09 13:01:21 +0100

    chore: upgrade `xjs` to v0.11.0

--- html/parser.go
+++ html/parser.go
@@ -3,16 +3,16 @@ package html
 import (
 	"github.com/xjslang/xjs/ast"
 	"github.com/xjslang/xjs/js"
+	"github.com/xjslang/xjs/jsextended"
 	"github.com/xjslang/xjs/parser"
-	"github.com/xjslang/xjs/plugin"
 	"github.com/xjslang/xjs/scanner"
 	"github.com/xjslang/xjs/token"
 )
 
 var (
-	startTag       = token.RegisterType("<")
-	endTag         = token.RegisterType("</")
-	selfClosingTag = token.RegisterType("/>")
+	START_TAG        = token.RegisterType("START_TAG", "<")
+	END_TAG          = token.RegisterType("END_TAG", "</")
+	SELF_CLOSING_TAG = token.RegisterType("SELF_CLOSING_TAG", "/>")
 )
 
 type Attr struct {
@@ -35,14 +35,14 @@ type Tag struct {
 
 func parseTag(p *parser.Parser) (node *Tag, err error) {
 	node = &Tag{}
-	if node.Layout.StartTag, err = p.Expect(startTag); err != nil {
+	if node.Layout.StartTag, err = p.Expect(START_TAG); err != nil {
 		return
 	}
 	if node.Name, err = js.ParseIdent(p); err != nil {
 		return
 	}
 	for {
-		if typ := p.CurrentToken.Type; typ == token.GT || typ == selfClosingTag {
+		if typ := p.CurrentToken.Type; typ == token.GT || typ == SELF_CLOSING_TAG {
 			break
 		}
 		var attr Attr
@@ -57,7 +57,7 @@ func parseTag(p *parser.Parser) (node *Tag, err error) {
 		}
 		node.Attrs = append(node.Attrs, attr)
 	}
-	if p.CurrentToken.Type == selfClosingTag {
+	if p.CurrentToken.Type == SELF_CLOSING_TAG {
 		node.SelfClosing = true
 		node.Layout.SelfClosingTag = p.CurrentToken
 		p.AdvanceToken()
@@ -65,14 +65,14 @@ func parseTag(p *parser.Parser) (node *Tag, err error) {
 	} else if _, err = p.Expect(token.GT); err != nil {
 		return
 	}
-	for p.CurrentToken.Type != endTag {
+	for p.CurrentToken.Type != END_TAG {
 		var child ast.Expr
 		if child, err = p.ParseExpr(); err != nil {
 			return
 		}
 		node.Children = append(node.Children, child)
 	}
-	if node.Layout.EndTag, err = p.Expect(endTag); err != nil {
+	if node.Layout.EndTag, err = p.Expect(END_TAG); err != nil {
 		return
 	}
 	var ident *js.Ident
@@ -92,11 +92,12 @@ func parseTag(p *parser.Parser) (node *Tag, err error) {
 }
 
 // Plugin enriches the JavaScript parser, so that we can parse expressions that are not part of the JS standard.
-func Plugin(b *plugin.Builder) {
-	token.RegisterUnaryType(startTag)
+func Parse(input []byte) (*js.Program, error) {
+	token.RegisterPrefixOp(START_TAG)
 
+	sb := jsextended.ScannerBuilder()
 	// now the parser can "scan" '<' and '</'
-	b.UseScanner(func(sc *scanner.Scanner, next func() (token.Token, error)) (tok token.Token, err error) {
+	sb.UseScanner(func(sc *scanner.Scanner, next func() (token.Token, error)) (tok token.Token, err error) {
 		if tok, err = next(); err != nil {
 			return
 		}
@@ -105,27 +106,29 @@ func Plugin(b *plugin.Builder) {
 			c := sc.CurrentChar()
 			switch {
 			case scanner.IsLetter(c):
-				tok.Type = startTag
+				tok.Type = START_TAG
 			case c == '/':
 				sc.AdvanceChar()
-				tok.Type = endTag
+				tok.Type = END_TAG
 				tok.Literal = "</"
 			}
 		case token.DIVIDE:
 			if sc.CurrentChar() == '>' {
 				sc.AdvanceChar()
-				tok.Type = selfClosingTag
+				tok.Type = SELF_CLOSING_TAG
 				tok.Literal = "/>"
 			}
 		}
 		return
 	})
-
-	// now the parser can "parse" HTML tags
-	b.UseUnaryParser(func(p *parser.Parser, next func() (ast.Expr, error)) (_ ast.Expr, err error) {
-		if p.CurrentToken.Type == startTag {
+	pb := jsextended.ParserBuilder()
+	pb.UsePrefixOpParser(func(p *parser.Parser, next func() (ast.Expr, error)) (_ ast.Expr, err error) {
+		if p.CurrentToken.Type == START_TAG {
 			return parseTag(p)
 		}
 		return next()
 	})
+	s := sb.Build(input)
+	p := pb.Build(s)
+	return js.ParseProgram(p)
 }
